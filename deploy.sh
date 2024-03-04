3e5041dfe07e5add41f7c99cb1c1c42e67708c03
Andrii Nugged <nugged@gmail.com>
2024-03-04 02:42:52 +0200

    Add release action for GitHub.
    Align with latest bywatersolutions/koha-plugin-kitchen-sink changes

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-PM_FILE="Koha/Plugin/Fi/KohaSuomi/Editx.pm"
-VERSION=`grep -oE "\-?[0-9]+\.[0-9]+\.[0-9]" $PM_FILE | head -1`
-RELEASE_FILE="koha-plugin-editx-v${VERSION}.kpz"
-rm 
-echo "Building release package ${RELEASE_FILE}"
-zip -r $RELEASE_FILE ./Koha
