196c08d2400fd8bea4ad2ed7c6e21f856773828f
Andrii Nugged <nugged@gmail.com>
2026-05-06 12:02:53 +0300

    Add EDItX mapping configuration

--- Koha/Plugin/Fi/KohaSuomi/Editx.pm
+++ Koha/Plugin/Fi/KohaSuomi/Editx.pm
@@ -6,6 +6,7 @@ use base qw(Koha::Plugins::Base);
 ## We will also need to include any Koha libraries we want to access
 use C4::Context;
 use Koha::DateUtils qw(dt_from_string);
+use Text::CSV_XS;
 use utf8;
 ## Here we set our plugin version
 our $VERSION = "{VERSION}";
@@ -107,6 +108,48 @@ sub uninstall() {
     return $success;
 }
 
+sub configure {
+    my ( $self, $args ) = @_;
+    my $cgi = $self->{'cgi'};
+    my @messages;
+    my $saved;
+
+    $self->_install_or_upgrade_tables();
+
+    if ( $cgi->request_method eq 'POST' && $cgi->param('save') ) {
+        my $mapping_csv = $cgi->param('mapping_csv') // '';
+        my ( $rows, $parse_messages, $has_blocking_errors ) = $self->_parse_productform_mapping_csv($mapping_csv);
+        push @messages, @$parse_messages;
+
+        if ($has_blocking_errors) {
+            $self->_output_configure_page(
+                mapping_csv    => $mapping_csv,
+                messages       => \@messages,
+                saved          => 0,
+            );
+            return;
+        }
+
+        my $save_messages = $self->_save_productform_mappings($rows);
+        push @messages, @$save_messages;
+        if (@$save_messages) {
+            $self->_output_configure_page(
+                mapping_csv => $mapping_csv,
+                messages    => \@messages,
+                saved       => 0,
+            );
+            return;
+        }
+        $saved = 1;
+    }
+
+    $self->_output_configure_page(
+        mapping_csv => $self->_productform_mapping_csv(),
+        messages    => \@messages,
+        saved       => $saved,
+    );
+}
+
 sub _migrate_legacy_sequences_table {
     my ($self) = @_;
 
@@ -206,6 +249,216 @@ sub _ensure_sequences_row {
     return 1 if $count;
 
     return $dbh->do("INSERT INTO $sequences_table (invoicenumber, item_barcode_nextval) VALUES (0, 0)");
+}
+
+sub _output_configure_page {
+    my ( $self, %params ) = @_;
+
+    my $template = $self->get_template( { file => 'configure.tt' } );
+    $template->param(
+        mapping_csv    => $params{mapping_csv},
+        messages       => $params{messages},
+        saved          => $params{saved},
+        itemtypes_text => join( ', ', @{ $self->_itemtypes() } ),
+        last_upgraded  => $self->retrieve_data('last_upgraded'),
+    );
+
+    return $self->output_html( $template->output() );
+}
+
+sub _parse_productform_mapping_csv {
+    my ( $self, $mapping_csv ) = @_;
+
+    my $csv = Text::CSV_XS->new(
+        {
+            binary           => 1,
+            allow_whitespace => 1,
+            blank_is_undef   => 1,
+        }
+    );
+    my %itemtypes = map { $_ => 1 } @{ $self->_itemtypes() };
+    my ( @rows, @messages, %seen_onix_codes );
+    my $has_blocking_errors;
+    my $line_number = 0;
+
+    open my $fh, '<', \$mapping_csv or die "Cannot read product form mapping CSV: $!";
+
+    while ( my $fields = $csv->getline($fh) ) {
+        $line_number++;
+        next unless grep { defined $_ && $_ ne '' } @$fields;
+        next if $line_number == 1 && $self->_is_productform_mapping_csv_header($fields);
+
+        if ( @$fields != 3 ) {
+            push @messages, $self->_configure_message( error => "Line $line_number has " . scalar(@$fields) . " columns; expected 3." );
+            $has_blocking_errors = 1;
+            next;
+        }
+
+        my ( $onix_code, $productform, $productform_alternative ) = map { $self->_trim_csv_value($_) } @$fields;
+
+        unless ($onix_code) {
+            push @messages, $self->_configure_message( error => "Line $line_number has no ONIX code." );
+            $has_blocking_errors = 1;
+            next;
+        }
+
+        if ( $seen_onix_codes{$onix_code}++ ) {
+            push @messages, $self->_configure_message( warning => "Line $line_number repeats ONIX code '$onix_code'; the later value will win." );
+        }
+
+        if ( $productform && !$itemtypes{$productform} ) {
+            push @messages, $self->_configure_message( warning => "Line $line_number: item type '$productform' does not exist; productform was stored as NULL." );
+            $productform = undef;
+        }
+
+        if ( $productform_alternative && !$itemtypes{$productform_alternative} ) {
+            push @messages, $self->_configure_message( warning => "Line $line_number: item type '$productform_alternative' does not exist; productform_alternative was stored as NULL." );
+            $productform_alternative = undef;
+        }
+
+        push @rows,
+            {
+                onix_code               => $onix_code,
+                productform             => $productform,
+                productform_alternative => $productform_alternative,
+            };
+    }
+
+    if ( !$csv->eof ) {
+        my ( $code, $message, $position ) = $csv->error_diag();
+        push @messages, $self->_configure_message( error => "CSV parse failed at line $line_number, position $position: $message ($code)." );
+        $has_blocking_errors = 1;
+    }
+
+    close $fh;
+
+    unless (@rows) {
+        push @messages, $self->_configure_message( error => 'No product form mappings found in CSV.' );
+        $has_blocking_errors = 1;
+    }
+
+    return ( \@rows, \@messages, $has_blocking_errors );
+}
+
+sub _save_productform_mappings {
+    my ( $self, $rows ) = @_;
+
+    my $dbh = C4::Context->dbh;
+    my $map_productform_table = $self->_quote_identifier( $self->get_qualified_table_name('map_productform') );
+    my @messages;
+
+    my $saved = eval {
+        $dbh->begin_work;
+        $dbh->do("DELETE FROM $map_productform_table") or die $dbh->errstr;
+
+        my $sth = $dbh->prepare( "
+            INSERT INTO $map_productform_table (onix_code, productform, productform_alternative)
+            VALUES (?, ?, ?)
+            ON DUPLICATE KEY UPDATE
+                productform = VALUES(productform),
+                productform_alternative = VALUES(productform_alternative)
+        " );
+
+        for my $row (@$rows) {
+            $sth->execute( $row->{onix_code}, $row->{productform}, $row->{productform_alternative} ) or die $dbh->errstr;
+        }
+
+        $dbh->commit;
+        1;
+    };
+
+    if ( !$saved ) {
+        my $error = $@ || $dbh->errstr;
+        eval { $dbh->rollback };
+        push @messages, $self->_configure_message( error => "Could not save product form mappings: $error" );
+    }
+
+    return \@messages;
+}
+
+sub _productform_mapping_csv {
+    my ($self) = @_;
+
+    my $dbh = C4::Context->dbh;
+    my $map_productform_table = $self->_quote_identifier( $self->get_qualified_table_name('map_productform') );
+    my $sth = $dbh->prepare( "
+        SELECT onix_code, productform, productform_alternative
+        FROM $map_productform_table
+        ORDER BY onix_code
+    " );
+    $sth->execute();
+
+    my $csv = Text::CSV_XS->new(
+        {
+            binary => 1,
+            eol    => "\n",
+        }
+    );
+    my $mapping_csv = '';
+
+    open my $fh, '>', \$mapping_csv or die "Cannot write product form mapping CSV: $!";
+    $csv->print( $fh, [qw(onix_code productform productform_alternative)] );
+
+    while ( my $row = $sth->fetchrow_hashref ) {
+        $csv->print(
+            $fh,
+            [
+                $row->{onix_code}               // '',
+                $row->{productform}             // '',
+                $row->{productform_alternative} // '',
+            ]
+        );
+    }
+
+    close $fh;
+
+    return $mapping_csv;
+}
+
+sub _itemtypes {
+    my ($self) = @_;
+
+    my $dbh = C4::Context->dbh;
+    my $sth = $dbh->prepare('SELECT itemtype FROM itemtypes ORDER BY itemtype');
+    $sth->execute();
+
+    my @itemtypes;
+    while ( my ($itemtype) = $sth->fetchrow_array ) {
+        push @itemtypes, $itemtype;
+    }
+
+    return \@itemtypes;
+}
+
+sub _is_productform_mapping_csv_header {
+    my ( $self, $fields ) = @_;
+
+    return unless @$fields == 3;
+
+    my @header = map { lc( $self->_trim_csv_value($_) // '' ) } @$fields;
+    return $header[0] eq 'onix_code'
+        && $header[1] eq 'productform'
+        && $header[2] eq 'productform_alternative';
+}
+
+sub _trim_csv_value {
+    my ( $self, $value ) = @_;
+
+    return unless defined $value;
+
+    $value =~ s/\A\x{FEFF}//;
+    $value =~ s/\A\s+|\s+\z//g;
+    return $value eq '' || uc($value) eq 'NULL' ? undef : $value;
+}
+
+sub _configure_message {
+    my ( $self, $type, $text ) = @_;
+
+    return {
+        type        => $type,
+        alert_class => $type eq 'error' ? 'danger' : $type,
+        text        => $text,
+    };
 }
 
 sub _table_exists {
